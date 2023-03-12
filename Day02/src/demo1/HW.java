3f92ce81af5a11d6bf1ae71d822c062001e71aac
Chris_Shaw <1207824422@qq.com>
2023-03-12 16:34:58 +0800

    inital try git

---
+++ Day02/src/demo1/HW.java
@@ -0,0 +1,10 @@
+package demo1;
+import java.util.Scanner;
+public class HW {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i1 = sc.nextInt();
+        int i2 = sc.nextInt();
+        System.out.println(i1+i2);
+    }
+}
